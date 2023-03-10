2a0127e8a5f70ace4fd44a01c40feaffec6e0aa4
Tanveer88-88 <sitz_tani@hotmail.com>
2023-03-10 16:53:22 +0500

    header commit

--- resume-next13-tailweind/components/Header.tsx
+++ resume-next13-tailweind/components/Header.tsx
@@ -1,13 +1,39 @@
 import Wraper from "@/shared/Wraper";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <Wraper>
-    <header>
-        <p>Tanveer Mobeen</p>
+   <Wraper>
+    <header className="text-[#21343b]">
+        <section className="flex justify-between">
+          <p className="text-base py-7 tracking-widest">MADDIN HARRIS</p>
+          <List/>
+          <button id="hamburger-button" className="text-3xl cursor-pointer relative">
+                    &#9776;
+                    {/* <div className="bg-white w-8 h-1 rounded absolute top-4 -mt-0.5 transition-all duration-500 before:content-[''] before:bg-white before:w-8 before:h-1 before:rounded before:absolute before:-translate-x-4 before:-translate-y-3 before:transition-all before:duration-500 after:content-[''] after:bg-white after:w-8 after:h-1 after:rounded after:absolute after:-translate-x-4 after:translate-y-3 after:transition-all after:duration-500"></div> */}
+
+          </button>
+        </section>
+        <section id="mobile-menu">
+
+        </section>
     </header>
     </Wraper>
   )
 }
 
 export default Header
+
+export const List = () => {
+  return (
+    <div>
+       <ul className="flex text-lg font-serif tracking-widest"id="menu-list">
+            <li className=" hover:bg-[#e2c53d] py-6 px-4"><Link href={"#home"}>HOME</Link></li>
+            <li className=" hover:bg-[#e2c53d] py-6 px-4"><Link href={"#home"}>ABOUT</Link></li>
+            <li className=" hover:bg-[#e2c53d] py-6 px-4"><Link href={"#home"}>SKILLS</Link></li>
+            <li className=" hover:bg-[#e2c53d] py-6 px-4"><Link href={"#home"}>PORTFOLIO</Link></li>
+            <li className=" hover:bg-[#e2c53d] py-6 px-4"><Link href={"#home"}>CONTACT</Link></li>
+          </ul>
+    </div>
+  )
+}
--- resume-next13-tailweind/shared/Wraper.tsx
+++ resume-next13-tailweind/shared/Wraper.tsx
@@ -1,8 +1,6 @@
-import React from "react"
-
 const Wraper = ({children}:{children:React.ReactNode}) => {
   return (
-    <div className="max-w-2xl mx-auto px-4">
+    <div className="max-w-full ml-24 mr-8">
         {children}
         </div>
   )
